f59988cb39bfb6695294b17033ab583508c69fe9
VitaliiS-983 <42441367+VitaliiS-983@users.noreply.github.com>
2020-07-23 17:37:31 +0300

    Update alerta-frontend-entrypoint.sh

--- alerta-frontend-entrypoint.sh
+++ alerta-frontend-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Generate web console config, if not supplied
 if [ ! -f "${ALERTA_WEB_CONF_FILE}" ]; then
-  envsubst /web/config.json
+  envsubst < /usr/share/nginx/html/config.json.template > /usr/share/nginx/html/config.json
 fi
 
 exec "$@"
